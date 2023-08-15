55d620a06a3f722c86076452c7e60f7946e57dec
toyman640 <falakoadebayor@gmail.com>
2023-08-15 16:25:07 +0100

    Fix Game class

--- classes/game.rb
+++ classes/game.rb
@@ -5,9 +5,10 @@ class Game < Item
   attr_accessor
 
   def initialize(multiplayer, last_played)
-    super('2000-04-01')
+    super('2-12-2000')
     @multiplayer = multiplayer
     @last_played = last_played
+    @publish_date = publish_date
   end
 
   def can_be_archived?
