571ac328d8f0932487c88a6969af938ff20edc41
toyman640 <falakoadebayor@gmail.com>
2023-08-16 00:30:59 +0100

    Fix linters errors

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,16 +1,15 @@
 require_relative '../classes/author'
 
 describe Author do
-  it "it should take first_name and last_name arguments" do
-    author_1 = Author.new("John", "Doe")
-    expect("#{author_1.first_name} #{author_1.last_name}").to eq "John Doe"
+  it 'it should take first_name and last_name arguments' do
+    first_author = Author.new('John', 'Doe')
+    expect("#{first_author.first_name} #{first_author.last_name}").to eq 'John Doe'
   end
 
-  it "Item array to take inputs" do
-    author_2 = Author.new("Jane", "Doe")
-    item = Item.new("2022-01-01") 
-    author_2.add_item(item)
-    expect(author_2.instance_variable_get(:@items).length).to eq 1
+  it 'Item array to take inputs' do
+    second_author = Author.new('Jane', 'Doe')
+    item = Item.new('2022-01-01')
+    second_author.add_item(item)
+    expect(second_author.instance_variable_get(:@items).length).to eq 1
   end
-  
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,28 +1,27 @@
 require_relative '../classes/game'
 
 describe Game do
-
-  it "should return true" do
-    game_1 = Game.new("Yes", "2007-02-11")
+  it 'should return true' do
+    new_game = Game.new('Yes', '2007-02-11')
     allow_any_instance_of(Item).to receive(:can_be_archived?).and_return(true)
-    expect(game_1.can_be_archived?).to be true
+    expect(new_game.can_be_archived?).to be true
   end
 
-  it "should return true" do
-    game_1 = Game.new("Yes", "2022-02-11")
+  it 'should return true' do
+    new_game = Game.new('Yes', '2022-02-11')
     allow_any_instance_of(Item).to receive(:can_be_archived?).and_return(true)
-    expect(game_1.can_be_archived?).to be false
+    expect(new_game.can_be_archived?).to be false
   end
 
-  it "should return true" do
-    game_1 = Game.new("Yes", "2014-02-11")
+  it 'should return true' do
+    new_game = Game.new('Yes', '2014-02-11')
     allow_any_instance_of(Item).to receive(:can_be_archived?).and_return(false)
-    expect(game_1.can_be_archived?).to be false
+    expect(new_game.can_be_archived?).to be false
   end
 
-  it "should return true" do
-    game_1 = Game.new("Yes", "2022-02-11")
+  it 'should return true' do
+    new_game = Game.new('Yes', '2022-02-11')
     allow_any_instance_of(Item).to receive(:can_be_archived?).and_return(false)
-    expect(game_1.can_be_archived?).to be false
+    expect(new_game.can_be_archived?).to be false
   end
 end
