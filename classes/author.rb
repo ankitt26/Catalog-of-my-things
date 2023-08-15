bdcca3b8a1d00c4abf6f7d07db0c4ab52484b27c
toyman640 <falakoadebayor@gmail.com>
2023-08-15 15:50:49 +0100

    Create author and game classes

---
+++ classes/author.rb
@@ -0,0 +1,18 @@
+require_relative 'item'
+
+class Author
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+end
---
+++ classes/game.rb
@@ -0,0 +1,22 @@
+require_relative 'item'
+require 'date'
+
+class Game < Item
+  attr_accessor 
+
+  def initialize(multiplayer, last_played)
+    super('2000-04-01')
+    @multiplayer = multiplayer
+    @last_played = last_played
+  end
+
+  def can_be_archived?
+    current_date = Date.today.year
+    last_played = Date.parse(@last_played).year
+    game_archivable = current_date - last_played > 2
+    super && game_archivable 
+  end
+end
+
+game_1 = Game.new('Yes', '2009-01-12')
+puts game_1.can_be_archived?
