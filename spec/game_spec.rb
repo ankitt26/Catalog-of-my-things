54a1bc1cc7d949a41462ceaa8c925c17c565c7ec
toyman640 <falakoadebayor@gmail.com>
2023-08-16 15:28:22 +0100

    Fix linters error

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require_relative '../classes/game'
 
 describe Game do
   it 'should restructure date format correctly' do
-    new_game = Game.new('2020-2-13','Yes', '11/02/2015')
+    new_game = Game.new('2020-2-13', 'Yes', '11/02/2015')
     expected_date = Date.new(2015, 2, 11)
     expect(new_game.instance_variable_get(:@last_played_at)).to eq expected_date
   end
