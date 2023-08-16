98cbdb58ec36da5626a732bce604710f51c9b368
toyman640 <falakoadebayor@gmail.com>
2023-08-16 06:20:44 +0100

    Fix linters error

--- classes/game.rb
+++ classes/game.rb
@@ -2,7 +2,7 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
-  attr_accessor :multiplayer,  :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at)
     super(publish_date)
