8ca24c6685c2d2d59e6f183a8f0fd8df616e53de
forLoop94 <charlesuwem94@gmail.com>
2023-08-16 13:20:07 +0100

    Add method to get label and author in the App class

--- app.rb
+++ app.rb
@@ -19,4 +19,31 @@ class App
     data.save(file, genre.to_hash)
     genre.summary
   end
+
+  def get_author(file, item)
+    puts 'The Author?'
+    print 'First name: '
+    first_name = gets.chomp.to_s
+    print 'last name: '
+    last_name = gets.chomp.to_s
+
+    author = Author.new(first_name, last_name)
+    item.add_author(author)
+    data = SaveRetrieveData.new
+    data.save(file, author.to_hash)
+    author.summary
+  end
+
+  def get_label(file, item)
+    puts 'The Label?'
+    print 'Title: '
+    title = gets.chomp.to_s
+    print 'Color: '
+    color = gets.chomp.to_s
+    label = Label.new(title, color)
+    item.add_label(label)
+    data = SaveRetrieveData.new
+    data.save(file, label.to_hash)
+    label.summary
+  end
 end
