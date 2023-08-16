9f49d27e700645c0ccb24e5a920d6cfa8431e9c9
toyman640 <falakoadebayor@gmail.com>
2023-08-16 15:07:04 +0100

    Hash game values

--- classes/game.rb
+++ classes/game.rb
@@ -2,13 +2,21 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(multiplayer, last_played_at)
+  def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
     @publish_date = publish_date
+  end
+
+  def to_hash
+    {
+      'publish_date' => @publish_date,
+      'multiplayer' => @multiplayer,
+      'last_played' => @last_played_at
+    }
   end
 
   private
