cd0c0953993f42dc290cdc05fd4c4a1063d2727c
forLoop94 <charlesuwem94@gmail.com>
2023-08-17 10:32:23 +0100

    In main, get user choice and call corresponding App class method

--- main.rb
+++ main.rb
@@ -18,6 +18,28 @@ def main
   puts '**********************************************'
   puts "\nCATALOG OF MY THINGS\n\n"
   puts '**********************************************'
+  loop do
+    puts 'Select an option to continue'
+    OPTIONS.each { |index, string| puts "#{index} - #{string}" }
+    user_option = gets.chomp.to_s
+    case user_option
+    when '1' then app.list_books
+    when '2' then app.list_music_albums
+    when '3' then app.list_games
+    when '4' then app.list_genres
+    when '5' then app.list_labels
+    when '6' then app.list_authors
+    when '7' then app.add_book
+    when '8' then app.add_music_album
+    when '9' then app.add_game
+    when '10'
+      puts "Thanks for using this app"
+      puts "\n***********************************************"
+      exit
+    else
+      puts 'Invalid option, please select a number between 1 and 10'
+    end
+  end
 end
 
 main
